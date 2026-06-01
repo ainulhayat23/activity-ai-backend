26cbd9cc99f316890fc6aa9fff9a070b43f70abe
yayat <146690579+ainulhayat23@users.noreply.github.com>
2026-06-02 07:26:19 +0800

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -43,7 +43,7 @@ Gunakan bahasa Indonesia yang sederhana, rapi, dan mudah dipahami.
     `;
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+     `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
       {
         method: "POST",
         headers: {
